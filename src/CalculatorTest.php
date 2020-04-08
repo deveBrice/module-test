b5eb1fe5d84d3b5ea7094aa9e6c5032c12953f98
brice <brice.william971@gmail.com>
2020-04-08 15:51:22 +0200

    add of other test

--- src/CalculatorTest.php
+++ src/CalculatorTest.php
@@ -20,4 +20,15 @@ class CalculatorTests extends PHPUnit_Framework_TestCase
         $this->assertEquals(3, $result);
     }
  
+    public function testEmpty() 
+    {
+       $result = $this->calculator->add(1, 2);
+       $this->assertEmpty($result);
+    }
+
+    public function type()
+    {
+        $result = $this->calculator->add(1, 2);
+        $this->assertIsInt($result);
+    }
 }
